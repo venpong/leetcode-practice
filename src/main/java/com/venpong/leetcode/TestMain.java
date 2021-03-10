9ec259424bff6a5770dfadae7b65da25eeedfa08
venpong <venpong.x@gmail.com>
2021-03-10 09:57:16 +0800

    first commit

---
+++ src/main/java/com/venpong/leetcode/TestMain.java
@@ -0,0 +1,12 @@
+package com.venpong.leetcode;
+
+/**
+ * @author: venpong
+ * @date: 2021/3/10 9:54 上午
+ * @description:
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
